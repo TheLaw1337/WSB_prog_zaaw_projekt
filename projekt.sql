8d72f62d819a485f9754365315c95bb713251e1e
Daniel Sobczak <20947896+TheLaw1337@users.noreply.github.com>
2020-12-12 01:11:23 +0100

    SQL file update

--- projekt.sql
+++ projekt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 16 Cze 2020, 17:50
+-- Czas generowania: 12 Gru 2020, 01:10
 -- Wersja serwera: 10.4.11-MariaDB
 -- Wersja PHP: 7.4.6
 
@@ -41,14 +41,15 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`EventId`, `Name`, `Description`, `PlaceID`, `TypeID`, `Date`) VALUES
-(1, 'Parasite w Kinie Muza', 'Pokaz głośnego filmu Oscarowego \"Parasite\" w Kinie Muza ', 1, 1, '2020-06-10 20:00:00'),
-(6, 'Black Marble - koncert', 'Drugi koncert zespołu Black Marble w Poznaniu', 2, 2, '2020-06-29 19:00:00'),
-(7, 'Noc Muzeów w Muzeum Sztuki', 'Muzeum Sztuki po raz kolejny zaprasza na udział w dorocznej Nocy Muzeów', 3, 3, '2020-07-02 10:00:00'),
-(8, '\"Skąpiec\" w Teatrze Nowym', 'Kultowa sztuka Moliera wraca do Teatru Nowego', 4, 4, '2020-06-23 18:00:00'),
-(9, 'Noc Horrorów', 'Maraton klasycznych reprezentantów filmowego horroru. \"Koszmar z Ulicy Wiązów\", \"Coś\", \"Halloween\" i inne', 5, 1, '2020-08-10 22:00:00'),
-(10, 'Koncert w Arenie', 'Koncert niespodzianka. Dalsze szczegóły wkrótce.', 6, 2, '2020-06-30 17:00:00'),
-(11, 'Mecz Pogoń Szczecin - Lech Poznań', 'Pogoń Szczecin podejmuje u siebie Kolejorza', 7, 5, '2020-07-06 18:00:00'),
-(12, 'Gala Boksu Zawodowego', 'Noc pełna wrażeń dostarczanych przez emocjonujące walki weteranów ringu', 7, 5, '2020-09-05 01:29:20');
+(1, 'Diuna w Kinie Muza', 'Pokaz najbardziej oczekiwanego filmu roku \"Diuna\" w Kinie Muza', 1, 1, '2021-01-19 20:00:00'),
+(6, 'Molchat Doma - koncert', 'Trzeci koncert wielkiego post-punkowego odkrycia ostatnich lat - białoruskiego zespołu Molchat Doma', 2, 2, '2021-01-14 19:00:00'),
+(7, 'Noc Muzeów w Muzeum Sztuki w Warszawie', 'Muzeum Sztuki w Warszawie po raz kolejny zaprasza na udział w dorocznej Nocy Muzeów', 3, 3, '2021-02-06 10:00:00'),
+(8, '\"Skąpiec\" w Teatrze Nowym', 'Kultowa sztuka Moliera wraca do Teatru Nowego', 4, 4, '2021-02-12 18:00:00'),
+(9, 'Noc Horrorów', 'Maraton klasycznych reprezentantów filmowego horroru. \"Koszmar z Ulicy Wiązów\", \"Coś\", \"Halloween\" i inne', 5, 1, '2021-02-14 22:00:00'),
+(10, 'Koncert w Arenie', 'Koncert niespodzianka. Dalsze szczegóły wkrótce.', 6, 2, '2020-12-23 17:00:00'),
+(11, 'Mecz Pogoń Szczecin - Lech Poznań', 'Pogoń Szczecin podejmuje u siebie Kolejorza', 7, 5, '2021-01-03 18:00:00'),
+(12, 'Gala Boksu Zawodowego', 'Noc pełna wrażeń dostarczanych przez emocjonujące walki weteranów ringu', 7, 5, '2021-02-07 01:29:20'),
+(14, 'Spotkanie z lamą - wystawa', 'Chcesz zobaczyć lamę na żywo? Wpadnij do nas i ją pogłaskaj!', 11, 3, '2020-12-21 12:00:00');
 
 -- --------------------------------------------------------
 
@@ -67,7 +68,23 @@ CREATE TABLE `photos` (
 --
 
 INSERT INTO `photos` (`id`, `path`, `eventID`) VALUES
-(3, '/img/default.png', 11);
+(5, 'art_7.jpg', 7),
+(6, 'cinema_1.png', 1),
+(15, 'molchat_doma.jpg', 6),
+(16, 'skapiec.jpg', 8),
+(17, 'horror.png', 9),
+(18, 'stadion.jpg', 11),
+(19, 'box.jpg', 12),
+(20, '_1.jpg', 1),
+(21, 'cinema_1_1.png', 1),
+(22, 'skapiec_1.jpg', 1),
+(23, 'cinema_1_1.png', 1),
+(24, '_1.jpg', 1),
+(25, 'cinema_1_1.png', 1),
+(26, '_1.jpg', 1),
+(27, 'cinema_1_1.png', 1),
+(28, 'cinema_1_1.png', 1),
+(29, 'lama_14.jpg', 14);
 
 -- --------------------------------------------------------
 
@@ -94,7 +111,9 @@ INSERT INTO `places` (`PlaceID`, `City`, `Street`, `Number`, `PostalCode`) VALUE
 (4, 'Kraków', '3 Maja', 1, '30-062'),
 (5, 'Gdańsk', 'Lektykarska', 4, '80-831'),
 (6, 'Wrocław', 'Kazimierza Wielkiego', 19, '50-077'),
-(7, 'Szczecin', 'Narbutta', 50, '02-541');
+(7, 'Szczecin', 'Narbutta', 50, '02-541'),
+(11, 'Fikcja', 'Fikcyjna', 1, '00-000'),
+(12, 'Night City', 'Cyberpunkowa', 2077, '20-777');
 
 -- --------------------------------------------------------
 
@@ -138,7 +157,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`UserID`, `Name`, `Password`, `timestamp`) VALUES
 (3, 'test.testowy', '$2y$10$aOnQGAi8cyXNgBh0PhHfleOFaciy5HM0VE9WzsYNz15YNPT0XbBVW', '2020-06-12 15:07:05'),
 (4, 'jan.kowalski', '$2y$12$zL7hs9FRBmCsDtx.7SpIJ.oIvQoOoKDgvZtAT5Dwx9CuwLAOL/QXK', '2020-06-13 22:55:08'),
-(5, 'anna.nowak', '$2y$12$uMfosGGiZt..MlgZM4bmv..R.2sTRz1gfIajr5uOC45S28/w6VUFq', '2020-06-13 22:56:45');
+(5, 'anna.nowak', '$2y$12$uMfosGGiZt..MlgZM4bmv..R.2sTRz1gfIajr5uOC45S28/w6VUFq', '2020-06-13 22:56:45'),
+(6, 'daniel.sobczak', '$2y$10$IYMW5fmjruYWDGTxPc1fIeJOlpebPD7z8T2mymf0Hyh0P3OPlvtaG', '2020-12-06 21:26:16');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -185,19 +205,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `events`
 --
 ALTER TABLE `events`
-  MODIFY `EventId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `EventId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT dla tabeli `photos`
 --
 ALTER TABLE `photos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT dla tabeli `places`
 --
 ALTER TABLE `places`
-  MODIFY `PlaceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `PlaceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT dla tabeli `types`
@@ -209,7 +229,7 @@ ALTER TABLE `types`
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Ograniczenia dla zrzutów tabel
